66f5fbfe4ac210ed433a71dfc38130e3057704d8
lassepe <lasse.peters@mailbox.org>
2020-07-12 15:28:40 +0200

    Fix VegaLite backend

--- src/visualize.jl
+++ src/visualize.jl
@@ -5,20 +5,27 @@ struct GGPlotBackend <: DisplayBackend end
 struct VegaBackend <: DisplayBackend end
 
 function visualize(lines; backend = GGPlotBackend())
-    visualize_(backend, lines)
-end
-
-function visualize_(::GGPlotBackend, lines)
     line_data = line_dataframe(lines)
     color_property = hasproperty(line_data, :class) ? :class : nothing
+    visualize_(backend, line_data, color_property)
+end
+
+function visualize_(::GGPlotBackend, line_data, color_property)
     ggplot(line_data, aes(x = :x, y = :y, xend = :xend, yend = :yend, color = color_property)) +
     geom_segment()
 end
 
-function visualize_(::VegaBackend, lines)
-    lines |>
-    line_dataframe |>
-    @vlplot(:rule, width = 900, height = 400, x = :x, y = :y, x2 = :xend, y2 = :yend)
+function visualize_(::VegaBackend, line_data, color_property)
+    line_data |> @vlplot(
+        :rule,
+        width = 900,
+        height = 400,
+        x = :x,
+        y = :y,
+        x2 = :xend,
+        y2 = :yend,
+        color = color_property
+    )
 end
 
 #====================================== input data conversion =====================================#
@@ -27,7 +34,7 @@ function line_dataframe(lines::DataFrame)
     lines
 end
 
-function line_dataframe(lines, class=nothing)
+function line_dataframe(lines, class = nothing)
     map(lines) do l
         (x, y), (xend, yend) = l
         line_data = (; x, y, xend, yend)
