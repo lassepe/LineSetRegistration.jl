a0db17dcf177c2a4867ea145e43e755c8204e8cb
lassepe <lasse.peters@mailbox.org>
2020-07-12 16:05:30 +0200

    Setup optimization utilities
    - introduce `line_fit_error` for the optimization objective (cost function)
    - introduce `pose_transformation` for the decision variable

--- src/main.jl
+++ src/main.jl
@@ -12,24 +12,22 @@ using VegaLite
 include("visualize.jl")
 include("spl_field_model.jl")
 
+function transform(tform, line)
+    Line(tform.(Tuple(line))...)
+end
+
 "The known lines on the map."
 spl_field = SPLField()
 "Some perceived lines."
-perceived_lines = [Line(rand(Point{2}), rand(Point{2})) for _ in 1:2]
+perceived_lines = [Line(Point(1, 0), Point(2, 0)), Line(Point(2, 0), Point(2, 2))]
 
-visualize(vcat(
-    line_dataframe(spl_field.lines, "map"),
-    line_dataframe(perceived_lines, "perceived"),
-)) |> display
-
-#====================================== optimization problem ======================================#
-
+#======================================= optimization utils =======================================#
 
 function line_fit_error(perceived_lines, map_lines)
     # technically, the right thing would be to comupte the sum of all distances between the
     # `perceived_lines` and the respective closest field `map_lines`. Thus, this would boild down
     # to the absolute area bewetten the perceived lines and their respective closeset field line.
-
+    #
     # for simplicty, let's just use the distnace of the end-points of the perceived lines to a field
     # line.
 
@@ -39,10 +37,14 @@ function line_fit_error(perceived_lines, map_lines)
         abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1) / norm(p1 - p2)
     end
 
-    function line_fit_error(line1::Line, line2::Line)
-        d1 = distance(line1[1], line2)
-        d2 = distance(line1[2], line2)
-        d1 + d2
+    function line_length(line::Line)
+        p1, p2 = line
+        norm(p1 - p2)
+    end
+
+    function line_fit_error(perceived_line::Line, map_line::Line)
+        d1, d2 = distance(perceived_line[1], map_line), distance(perceived_line[2], map_line)
+        line_length(perceived_line) * (d1 + d2) / 2
     end
 
     sum(perceived_lines) do perceived_line
@@ -51,3 +53,16 @@ function line_fit_error(perceived_lines, map_lines)
         end
     end
 end
+
+function pose_transformation(Δx, Δy, Δα)
+    sα, cα = sin(Δα), cos(Δα)
+    Translation(Δx, Δy) ∘ LinearMap(SMatrix{2,2}(cα, sα, -sα, cα))
+end
+
+transformed_lines = map(l -> transform(pose_transformation(1, 1, pi/4), l), perceived_lines)
+
+visualize(vcat(
+    line_dataframe(spl_field.lines, "map"),
+    line_dataframe(perceived_lines, "perceived"),
+    line_dataframe(transformed_lines, "transformed"),
+)) |> display
