da15db4f1da7917f9ce4fbf0cb05b694db17bfee
lassepe <lasse.peters@mailbox.org>
2020-09-11 14:29:10 +0200

    Improve test data generation

--- src/TestDataGenerator.jl
+++ src/TestDataGenerator.jl
@@ -10,18 +10,16 @@ import ..SPLFieldModel
 # include("GeometryTransformationUtils.jl")
 using ..GeometryTransformationUtils: line_vector, pose_transformation
 
-
 "Artificial noise on lines."
 function noisify(line; max_n_segments = 3, rng = Random.GLOBAL_RNG, σx = 0.01, σy = 0.01)
     n_segments = rand(1:max_n_segments)
     line_vec, p1, p2 = line_vector(line)
     line_segement_thresholds = vcat(0.0, sort(rand(rng, n_segments - 1)), 1.0)
-
-    rand_translation(rng) = Translation(σx * randn(rng), σy * randn(rng))
+    rand_translation() = Translation(σx * randn(rng), σy * randn(rng))
 
     map(line_segement_thresholds, vcat(line_segement_thresholds[2:end])) do t_start, t_end
-        p_start = rand_translation(rng)(p1 + t_start * line_vec)
-        p_end = rand_translation(rng)(p1 + t_end * line_vec)
+        p_start = rand_translation()(p1 + t_start * line_vec)
+        p_end = rand_translation()(p1 + t_end * line_vec)
         Line(p_start, p_end)
     end
 end
@@ -31,13 +29,14 @@ Sample up to `max_n_lines` from `spl_field.lines` (without replacement) and appl
 `rigid_transformation` and a artificial noise to each line.
 """
 function generate_lines(
-    max_n_lines = 10;
+    n_lines = 2:5,
+    pose_noise = (0.5, 0.5, deg2rad(15)),
     spl_field = SPLFieldModel.SPLField(),
     rng = GLOBAL_RNG,
     kwargs...,
 )
-    lines = Iterators.take(shuffle(rng, spl_field.lines), rand(rng, 1:max_n_lines))
-    rigid_tform = pose_transformation(randn(rng, 3))
+    lines = Iterators.take(shuffle(rng, spl_field.lines), rand(rng, n_lines))
+    rigid_tform = pose_transformation(2 * (rand(rng, 3) .- 0.5) .* pose_noise)
     mapreduce(vcat, lines) do l
         noisify(rigid_tform(l); rng, kwargs...)
     end
